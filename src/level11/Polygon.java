f8c37abf2e0d3bb9fe9cb2e724a9acf330307c6c
firenive <firenive@gmail.com>
2021-03-05 00:36:21 +0300

    level12

---
+++ src/level11/Polygon.java
@@ -0,0 +1,32 @@
+package level11;
+
+public class Polygon {
+    public static void main(String[] args) {
+        System.out.println(getShapeNameByCountOfCorners(3));
+        System.out.println(getShapeNameByCountOfCorners(5));
+        System.out.println(getShapeNameByCountOfCorners(1));
+
+    }
+
+    public static String getShapeNameByCountOfCorners(int countOfCorner) {
+
+/*        String shape = switch (countOfCorner) {
+            case 3 ->
+                "Треугольник";
+            case 4 ->
+                "Четырехугольник";
+            case 5 ->
+                "Пятиугольник";
+            case 6 ->
+                "Шестиугольник";
+            case 7 ->
+                "Семиугольник";
+            case 8 ->
+                "Восьмиугольник";
+            default ->
+                 "Другая фигура";
+        };*/
+        //return shape;
+        return null;
+    }
+}
